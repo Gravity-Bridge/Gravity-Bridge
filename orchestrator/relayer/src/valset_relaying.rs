e606f8f5f0d626bafa114b93788409a9f3ade73a
Christian Borst <christian@althea.systems>
2021-07-15 17:45:14 -0400

    Enable valset relaying price awareness

--- orchestrator/relayer/src/valset_relaying.rs
+++ orchestrator/relayer/src/valset_relaying.rs
@@ -116,6 +116,72 @@ async fn valset_cost_error(
     }
 }
 
+async fn should_relay_valset(
+    valset: &Valset,
+    ethereum_key: &EthPrivateKey,
+    cost: GasCost,
+    web3: &Web3,
+) -> bool {
+    let mut should_relay;
+
+    let relaying_cost = cost.gas * cost.gas_price;
+    let pub_key = ethereum_key.to_public_key().unwrap();
+    let reward = valset.reward_amount.clone();
+    let token_in = valset.reward_token;
+    if token_in.is_none() {
+        info!("No reward token has been determined for the valset, not relaying!");
+        return false;
+    }
+    let token_in = token_in.unwrap();
+    // for now we always want weth
+    let token_out = *web30::amm::WETH_CONTRACT_ADDRESS;
+    // If we're being rewarded in weth, we can just compare cost of gas to reward and see if we're getting enough
+    if token_out == token_in {
+        // TODO: Give relayers a configuration option in this case
+        should_relay = reward > relaying_cost;
+    } else {
+        warn!(
+            "No potential reward for relaying valset: {:#?} - not relaying!",
+            valset
+        );
+        return false;
+    }
+    // TODO: Give relayers an option for this value
+    // TODO: Understand this value better and give relayers an option
+    let sqrt_price_limit_x96_uint160 = 0u8.into();
+
+    if !should_relay {
+        //
+        let value = web3
+            .get_uniswap_price(
+                pub_key,
+                token_in,
+                token_out,
+                None,
+                reward,
+                Some(sqrt_price_limit_x96_uint160),
+                None,
+            )
+            .await;
+        if value.is_err() {
+            info!(
+                "Not relaying because uniswap returned an error {}",
+                value.err().unwrap()
+            );
+        } else {
+            let val = value.unwrap();
+            // TODO: Give relayers a configuration option in this case too
+            should_relay = val > relaying_cost;
+            debug!(
+                "We will be relaying because value of reward in weth: {} > cost of gas {}",
+                val, relaying_cost
+            );
+        }
+    }
+
+    should_relay
+}
+
 #[allow(clippy::too_many_arguments)]
 // Performs relaying of a valid valset, if it is profitable to do so
 async fn relay_valid_valset(
@@ -127,6 +193,7 @@ async fn relay_valid_valset(
     gravity_id: String,
     ethereum_key: EthPrivateKey,
     timeout: Duration,
+    enable_relay_market: &bool,
 ) {
     let cost = ethereum_gravity::valset_update::estimate_valset_cost(
         &latest_cosmos_valset,
@@ -162,17 +229,30 @@ async fn relay_valid_valset(
                 / downcast_to_u128(one_eth()).unwrap() as f32
         );
 
-    let _res = send_eth_valset_update(
-        latest_cosmos_valset,
-        current_valset,
-        &latest_cosmos_confirmed,
-        web3,
-        timeout,
-        gravity_contract_address,
-        gravity_id,
-        ethereum_key,
-    )
-    .await;
+    let should_relay = if *enable_relay_market {
+        should_relay_valset(&latest_cosmos_valset, &ethereum_key, cost, web3).await
+    } else {
+        true // Default to relaying if the relay market is disabled
+    };
+
+    if should_relay {
+        let _res = send_eth_valset_update(
+            latest_cosmos_valset,
+            current_valset,
+            &latest_cosmos_confirmed,
+            web3,
+            timeout,
+            gravity_contract_address,
+            gravity_id,
+            ethereum_key,
+        )
+        .await;
+    } else {
+        info!(
+            "Not relaying valset {:?} because it is not profitable",
+            latest_cosmos_valset
+        );
+    }
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -187,7 +267,7 @@ pub async fn relay_valsets(
     gravity_contract_address: EthAddress,
     gravity_id: String,
     timeout: Duration,
-    _config: &RelayerConfig,
+    config: &RelayerConfig,
 ) {
     // we have to start with the current valset, we need to know what's currently
     // in the contract in order to determine if a new validator set is valid.
@@ -252,6 +332,7 @@ pub async fn relay_valsets(
             gravity_id,
             ethereum_key,
             timeout,
+            &config.valset_market_enabled,
         )
         .await;
     }
