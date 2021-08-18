fa9e9e84733904758b1b0444560dcacf3fb84693
Justin Kilpatrick <justin@althea.net>
2021-08-18 07:06:20 -0400

    Move all tests to run on ETH London
    This patch removes the Happy path test that ran against the London
    hardfork and upgrades all of our tests to use the London hardfork as
    their Ethereum testing platform. Now that the London fork has ocurred on
    mainnet we're never going to see pre-london conditions again on
    Ethereum.

--- tests/container-scripts/run-testnet.sh
+++ tests/container-scripts/run-testnet.sh
@@ -46,8 +46,6 @@ if [[ $TEST_TYPE == *"ARBITRARY_LOGIC"* ]]; then
     bash /gravity/tests/container-scripts/run-solidity-test-fork.sh $ALCHEMY_ID &
 elif [[ $TEST_TYPE == *"RELAY_MARKET"* ]]; then
     bash /gravity/tests/container-scripts/run-eth-fork.sh $ALCHEMY_ID &
-elif [[ $TEST_TYPE == *"LONDON"* ]]; then
-    bash /gravity/tests/container-scripts/run-eth-london.sh &
 else
     bash /gravity/tests/container-scripts/run-eth.sh &
 fi
