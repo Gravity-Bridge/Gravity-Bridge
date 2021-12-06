44cfb2d3d188cc1638f4aaca605fc5a356d22fa3
Justin Kilpatrick <justin@althea.net>
2021-12-06 16:40:01 -0500

    Increase finality block wait
    This increases the orchestrators wait for block finality from 6 to 13
    blocks based on an Ethereum blog discussing an attack made up of 25%
    of the networks hashing power.

--- orchestrator/orchestrator/src/ethereum_event_watcher.rs
+++ orchestrator/orchestrator/src/ethereum_event_watcher.rs
@@ -239,13 +239,19 @@ pub async fn check_for_events(
 /// Given an uncle every 2.8 minutes, a 6 deep reorg would be 2.8 minutes * (100^4) or one
 /// 6 deep reorg every 53,272 years.
 ///
+/// Of course the above assume that no mining attacks occur. Once we bring that potential into
+/// the equation the question becomes 'how much money'. There is no depth safe from infinite
+/// spending. Taking some source values from https://blog.ethereum.org/2016/05/09/on-settlement-finality/
+/// we will use 13 blocks providing a 1/1_000_000 chance of an attacker with 25% of network hash
+/// power succeeding
+///
 pub async fn get_block_delay(web3: &Web3) -> Uint256 {
     let net_version = get_net_version_with_retry(web3).await;
 
     match net_version {
         // Mainline Ethereum, Ethereum classic, or the Ropsten, Kotti, Mordor testnets
         // all POW Chains
-        1 | 3 | 6 | 7 => 6u8.into(),
+        1 | 3 | 6 | 7 => 13u8.into(),
         // Dev, our own Gravity Ethereum testnet, and Hardhat respectively
         // all single signer chains with no chance of any reorgs
         2018 | 15 | 31337 => 0u8.into(),
@@ -254,6 +260,6 @@ pub async fn get_block_delay(web3: &Web3) -> Uint256 {
         // on experience with operational issues
         4 | 5 => 10u8.into(),
         // assume the safe option (POW) where we don't know
-        _ => 6u8.into(),
+        _ => 13u8.into(),
     }
 }
