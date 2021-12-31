5507f0744ba9e66dad08cffb894782e3e5d14c84
Michael J.S <32441941+humanalgorithm@users.noreply.github.com>
2021-12-31 17:27:57 -0500

    Fix Mac M1 integration tests (#12)
    This patch adds a simple conditional that enables x86 emulation for the docker test runner.

--- tests/build-container.sh
+++ tests/build-container.sh
@@ -13,4 +13,12 @@ pushd $REPOFOLDER
 # Build base container
 git archive --format=tar.gz -o $DOCKERFOLDER/gravity.tar.gz --prefix=gravity/ HEAD
 pushd $DOCKERFOLDER
-docker build -t gravity-base .
+
+# setup for Mac M1 Compatibility 
+PLATFORM_CMD=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ -n $(sysctl -a | grep brand | grep "M1") ]]; then
+       echo "Setting --platform=linux/amd64 for Mac M1 compatibility"
+       PLATFORM_CMD="--platform=linux/amd64"; fi
+fi
+docker build -t gravity-base $PLATFORM_CMD .
