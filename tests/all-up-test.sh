c9047754f15936ac7ebc3aae08706ea9b4376f69
Max N <nikolaev.makc@gmail.com>
2022-02-18 10:22:26 +0200

    fix issue #79 skip relay market test if ALCHEMY_ID empty

--- tests/all-up-test.sh
+++ tests/all-up-test.sh
@@ -33,4 +33,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # Run new test container instance
+if [[ -z ${ALCHEMY_ID} ]] && [[ $TEST_TYPE == *"RELAY_MARKET"* ]] ; then
+echo "Skip $TEST_TYPE"
+elif [[ -z ${ALCHEMY_ID} ]] && [[ $TEST_TYPE == *"ARBITRARY_LOGIC"* ]] ; then
+echo "Skip $TEST_TYPE"
+else 
 docker run --name gravity_all_up_test_instance $PLATFORM_CMD --cap-add=NET_ADMIN -t gravity-base /bin/bash /gravity/tests/container-scripts/all-up-test-internal.sh $NODES $TEST_TYPE $ALCHEMY_ID
+fi
