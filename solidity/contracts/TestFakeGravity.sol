0e000e59f15b558a1ed6ed9eaee9b6fa3d7c7681
Michael Scotto <scottomj@gmail.com>
2022-02-22 21:29:09 +0000

    formatting

--- solidity/contracts/TestFakeGravity.sol
+++ solidity/contracts/TestFakeGravity.sol
@@ -8,14 +8,8 @@ contract TestFakeGravity is Gravity {
 
     constructor(
         bytes32 _gravityId,
-		// The validator set, not in valset args format since many of it's
-		// arguments would never be used in this case
-		address[] memory _validators,
-		uint256[] memory _powers
-    ) Gravity(
-        _gravityId,
-		_validators,
-		_powers
-        ) {
+        address[] memory _validators,
+        uint256[] memory _powers)
+        Gravity(_gravityId, _validators, _powers) {
     }
 }
