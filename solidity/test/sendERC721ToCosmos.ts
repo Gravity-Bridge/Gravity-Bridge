d5dfb492de686a4f421f51d895a012bdb84aa9d2
Michael Scotto <scottomj@gmail.com>
2022-02-24 04:41:36 +0000

    adding some throw tests for submitERC721

--- solidity/test/sendERC721ToCosmos.ts
+++ solidity/test/sendERC721ToCosmos.ts
@@ -6,13 +6,17 @@ import { deployContractsERC721 } from "../test-utils/deployERC721";
 import {
   examplePowers
 } from "../test-utils/pure";
-import { GravityERC721 } from "../typechain";
+import { Gravity, GravityERC721, TestERC721A } from "../typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
 
 chai.use(solidity);
 const { expect } = chai;
 
 async function runTest(opts: {
   wrongERC721Owner?: boolean;
+  ERC721NotInContract?: boolean;
+  ERC721NotExist?: boolean; 
 }) {
 
   // Prep and deploy contract
@@ -36,7 +40,28 @@ async function runTest(opts: {
   if (!opts.wrongERC721Owner){
     await testERC721.functions.approve(gravityERC721.address, 190);
   }
+    await firstCall(gravityERC721, testERC721, signers, gravity);
+    let secondERC721 = await getSecondERC721(testERC721, gravityERC721, opts.ERC721NotExist, opts.ERC721NotInContract);
+    if (!opts.ERC721NotExist && !opts.ERC721NotInContract) {
+      await testERC721.functions.approve(gravityERC721.address, secondERC721);
+    }
+    await secondCall(gravityERC721, testERC721, signers, gravity, secondERC721);
+}
 
+async function getSecondERC721(testERC721: TestERC721A, gravityERC721: GravityERC721,
+   ERC721NotExist?: boolean, ERC721NotInContract?: boolean) {
+let secondERC721 = 191;
+  if (ERC721NotExist) {
+      secondERC721 = 1000; 
+  }
+  else if(ERC721NotInContract) {
+    secondERC721 = 190; 
+  }
+  return secondERC721
+}
+
+async function firstCall(gravityERC721: GravityERC721, testERC721: TestERC721A,
+   signers: SignerWithAddress[], gravity: Gravity) {
   await expect(gravityERC721.functions["sendERC721ToCosmos(address,string,uint256)"](
     testERC721.address,
     ethers.utils.formatBytes32String("myCosmosAddress"),
@@ -51,32 +76,46 @@ async function runTest(opts: {
     expect((await testERC721.functions["ownerOf(uint256)"](190))[0]).to.equal(gravityERC721.address);
     expect((await gravity.functions.state_lastEventNonce())[0]).to.equal(1);
     expect((await gravityERC721.functions.state_lastERC721EventNonce())[0]).to.equal(2);
+}
 
-    await testERC721.functions.approve(gravityERC721.address, 191);
-    await expect(gravityERC721.functions["sendERC721ToCosmos(address,string,uint256)"](
+async function secondCall(gravityERC721: GravityERC721, testERC721: TestERC721A,
+  signers: SignerWithAddress[], gravity: Gravity, secondERC721: number){
+  await expect(gravityERC721.functions["sendERC721ToCosmos(address,string,uint256)"](
+    testERC721.address,
+    ethers.utils.formatBytes32String("myCosmosAddress"),
+    secondERC721
+  )).to.emit(gravityERC721, 'SendERC721ToCosmosEvent').withArgs(
       testERC721.address,
+      await signers[0].getAddress(),
       ethers.utils.formatBytes32String("myCosmosAddress"),
-      191
-    )).to.emit(gravityERC721, 'SendERC721ToCosmosEvent').withArgs(
-        testERC721.address,
-        await signers[0].getAddress(),
-        ethers.utils.formatBytes32String("myCosmosAddress"),
-        191, 
-        3
-      );
-      expect((await testERC721.functions["ownerOf(uint256)"](191))[0]).to.equal(gravityERC721.address);
-      expect((await gravity.functions.state_lastEventNonce())[0]).to.equal(1);
-      expect((await gravityERC721.functions.state_lastERC721EventNonce())[0]).to.equal(3);
+      secondERC721, 
+      3
+    );
+    expect((await testERC721.functions["ownerOf(uint256)"](secondERC721))[0]).to.equal(gravityERC721.address);
+    expect((await gravity.functions.state_lastEventNonce())[0]).to.equal(1);
+    expect((await gravityERC721.functions.state_lastERC721EventNonce())[0]).to.equal(3);
 }
 
 describe("sendERC721ToCosmos tests", function () {
-  it("works right", async function () {
+  it("happy path", async function () {
     await runTest({})
   });
 
   it("throws on Wrong NFT owner", async function () {
     await expect(runTest({ wrongERC721Owner: true })).to.be.revertedWith(
       "ERC721: transfer caller is not owner nor approved"
+    );
+  });
+
+  it("throws on NFT not in contract", async function () {
+    await expect(runTest({ ERC721NotInContract: true })).to.be.revertedWith(
+      "ERC721: transfer of token that is not own"
+    );
+  });
+
+  it("throws on nonexistent token", async function () {
+    await expect(runTest({ ERC721NotExist: true })).to.be.revertedWith(
+      "ERC721: operator query for nonexistent token"
     );
   });
 });
