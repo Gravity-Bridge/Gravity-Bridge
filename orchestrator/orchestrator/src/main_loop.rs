cd70db7b620d267796474e5a350ec91ac09c9707
stana-ethernal <94966829+stana-ethernal@users.noreply.github.com>
2022-03-11 13:03:11 +0100

    Resolve attestation order race conditon (#124)
    This patch resolves a race condition around the unhalt bridge governance vote. Previously if a new event was submitted during the time the bridge was waiting for the nonces to be reset the Orchestrator code would not correctly detect the rollback and would remain stuck trying to submit an out of order event.
    
    Also included are improvements to the unhalt bridge test that reproduce this behavior and confirm the fix. 
    
    Co-authored-by: dusan.maksimovic@ethernal.tech <dusan.maksimovic@ethernal.tech>

--- orchestrator/orchestrator/src/main_loop.rs
+++ orchestrator/orchestrator/src/main_loop.rs
@@ -13,6 +13,7 @@ use cosmos_gravity::{
         get_oldest_unsigned_valsets,
     },
     send::{send_batch_confirm, send_logic_call_confirm, send_valset_confirms},
+    utils::get_last_event_nonce_with_retry,
 };
 use deep_space::error::CosmosGrpcError;
 use deep_space::Contact;
@@ -175,6 +176,31 @@ pub async fn eth_oracle_main_loop(
             }
         }
 
+        // if the governance vote reset last event nonce sent by validator to some lower value, we can detect this
+        // by comparing last_event_nonce retrieved from the chain with last_checked_event saved by the orchestrator
+        // in order to reset last_checked_block and last_checked_event and continue from that point
+        let last_event_nonce: Uint256 = get_last_event_nonce_with_retry(
+            &mut grpc_client,
+            our_cosmos_address,
+            contact.get_prefix().clone(),
+        )
+        .await
+        .into();
+
+        if last_event_nonce < last_checked_event {
+            // validator went back in history
+            info!("Governance unhalt vote must have happened, resetting the block to check!");
+            last_checked_event = last_event_nonce;
+            last_checked_block = get_last_checked_block(
+                grpc_client.clone(),
+                our_cosmos_address,
+                contact.get_prefix(),
+                gravity_contract_address,
+                &web3,
+            )
+            .await;
+        }
+
         // Relays events from Ethereum -> Cosmos
         match check_for_events(
             &web3,
@@ -189,23 +215,14 @@ pub async fn eth_oracle_main_loop(
         .await
         {
             Ok(nonces) => {
-                // this output CheckedNonces is accurate unless a governance vote happens
+                // If the governance happened while check_for_events() was executing and there were no new event nonces,
+                // nonces.event_nonce would return lower value than last_checked_event. We want to keep last_checked_event
+                // value so it could be used in the next iteration to check if we should return to the
+                // earlier block and continue from that point. CheckedNonces is accurate unless a governance vote happens.
                 last_checked_block = nonces.block_number;
-                if last_checked_event > nonces.event_nonce {
-                    // validator went back in history
-                    info!(
-                        "Governance unhalt vote must have happened, resetting the block to check!"
-                    );
-                    last_checked_block = get_last_checked_block(
-                        grpc_client.clone(),
-                        our_cosmos_address,
-                        contact.get_prefix(),
-                        gravity_contract_address,
-                        &web3,
-                    )
-                    .await;
+                if nonces.event_nonce > last_checked_event {
+                    last_checked_event = nonces.event_nonce;
                 }
-                last_checked_event = nonces.event_nonce;
                 metrics_latest(last_checked_event.to_u64_digits()[0], "last_checked_event");
             }
             Err(e) => {
