03616309b33086d7b01fb577b8772c26e5f7416e
Max N <nikolaev.makc@gmail.com>
2022-09-04 00:16:34 +0300

    add OnAcknowledgementPacket error handler

--- module/x/icaauth/keeper/keeper.go
+++ module/x/icaauth/keeper/keeper.go
@@ -1,12 +1,15 @@
 package keeper
 
 import (
+	"fmt"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/icaauth/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
-
 	icacontrollerkeeper "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/keeper"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 type Keeper struct {
@@ -31,4 +34,9 @@ func NewKeeper(cdc codec.Codec, storeKey sdk.StoreKey, iaKeeper icacontrollerkee
 // ClaimCapability claims the channel capability passed via the OnOpenChanInit callback
 func (k *Keeper) ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) error {
 	return k.scopedKeeper.ClaimCapability(ctx, cap, name)
+}
+
+// Logger returns the application logger, scoped to the associated module
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
