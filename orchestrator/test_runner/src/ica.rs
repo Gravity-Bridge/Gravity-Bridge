802a11a7d626687fcdeac60f18c1e6642b0d0ec7
Max N <nikolaev.makc@gmail.com>
2022-09-30 13:49:30 +0300

    dec request fee

--- orchestrator/test_runner/src/ica.rs
+++ orchestrator/test_runner/src/ica.rs
@@ -424,7 +424,7 @@ pub async fn ica_test(
             .await
             .expect("Failed to get current eth valset");
     let mut send_request_batch_fee = get_fee(Some(token_to_send_to_eth.to_string()));
-    send_request_batch_fee.amount = 10_000_000u64.into();
+    send_request_batch_fee.amount = 1_000_000u64.into();
     send_request_batch(
         keys[0].orch_key,
         token_to_send_to_eth.clone(),
