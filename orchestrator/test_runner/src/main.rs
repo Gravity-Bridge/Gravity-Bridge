0e24d69da723d2ac1be774afc027836298d0b12b
Christian Borst <christianoborst@gmail.com>
2022-12-06 16:14:27 -0500

    WIP: The test

--- orchestrator/test_runner/src/main.rs
+++ orchestrator/test_runner/src/main.rs
@@ -23,7 +23,7 @@ use crate::tx_cancel::send_to_eth_and_cancel;
 use crate::upgrade::{upgrade_part_1, upgrade_part_2};
 use crate::utils::*;
 use crate::valset_rewards::valset_rewards_test;
-use clarity::{Address, PrivateKey as EthPrivateKey};
+use clarity::{PrivateKey as EthPrivateKey};
 use clarity::{Address as EthAddress, Uint256};
 use deep_space::coin::Coin;
 use deep_space::Address as CosmosAddress;
@@ -31,7 +31,7 @@ use deep_space::Contact;
 use deep_space::{CosmosPrivateKey, PrivateKey};
 use erc_721_happy_path::erc721_happy_path_test;
 use evidence_based_slashing::evidence_based_slashing;
-use gravity_proto::gravity::query_client::{QueryClient as GravityQueryClient, QueryClient};
+use gravity_proto::gravity::query_client::{QueryClient as GravityQueryClient};
 use happy_path::happy_path_test;
 use happy_path_v2::happy_path_test_v2;
 use lazy_static::lazy_static;
@@ -40,11 +40,11 @@ use orch_only::orch_only_test;
 use relay_market::relay_market_test;
 use std::{env, time::Duration};
 use tokio::time::sleep;
-use tonic::transport::Channel;
+
 use transaction_stress_test::transaction_stress_test;
 use unhalt_bridge::unhalt_bridge_test;
 use valset_stress::validator_set_stress_test;
-use web30::client::Web3;
+
 
 mod airdrop_proposal;
 mod batch_timeout;
