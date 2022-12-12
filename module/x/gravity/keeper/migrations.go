0f82c88dc0af9b59cc8a9cf22495d43f44301be8
Christian Borst <christianoborst@gmail.com>
2022-12-12 12:00:08 -0500

    Add Pleiades part 1 -> Pleiades part 2 migration
    In Pleiades part 2 we will be adding a new parameter:
    MinChainFeeBasisPoints for cross-bridge balance checks. The chain will
    panic on the first EndBlocker execution if we do not add a migration for the
    Params to add a default value and a ParamSetPair for this new param.
    
    We also added a genesis value, but it remains to be seen if that
    requires migration logic.

--- module/x/gravity/keeper/migrations.go
+++ module/x/gravity/keeper/migrations.go
@@ -1,7 +1,7 @@
 package keeper
 
 import (
-	v3 "github.com/Gravity-Bridge/Gravity-Bridge/module/x/gravity/migrations/v3"
+	v4 "github.com/Gravity-Bridge/Gravity-Bridge/module/x/gravity/migrations/v4"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -15,8 +15,11 @@ func NewMigrator(keeper Keeper) Migrator {
 	return Migrator{keeper: keeper}
 }
 
-// Migrate1to2 migrates from consensus version 1 to 2.
-func (m Migrator) Migrate2to3(ctx sdk.Context) error {
-	ctx.Logger().Info("Pleiades Upgrade: Enter Migrate2to3()")
-	return v3.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
+// Migrate3to4 migrates from consensus version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	ctx.Logger().Info("Pleiades Upgrade part 2: Enter Migrate3to4()")
+	newParams := v4.MigrateParams(ctx, m.keeper.paramSpace)
+	m.keeper.SetParams(ctx, newParams)
+	ctx.Logger().Info("Pleiades Upgrade part 2: Gravity module migration is complete!")
+	return nil
 }
