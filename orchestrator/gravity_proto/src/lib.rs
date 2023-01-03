4edd6bcbf0647f0a7d4984c535fe1d0c7b430cd4
Pham Tu <tubackkhoa@gmail.com>
2023-01-03 15:36:24 +0700

    update new version of cosmos and tonic, prost

--- orchestrator/gravity_proto/src/lib.rs
+++ orchestrator/gravity_proto/src/lib.rs
@@ -9,3 +9,47 @@ pub mod gravity {
     include!("prost/gravity.v1.rs");
     include!("ethereum_claim.rs");
 }
+
+/// Bech32ibc protobuf definitions
+#[cfg(feature = "bech32ibc")]
+#[cfg_attr(docsrs, doc(cfg(feature = "bech32ibc")))]
+pub mod bech32ibc {
+    /// Bech32 prefix -> IBC Channel mapping
+    pub mod bech32ibc {
+        pub mod v1 {
+            include!("bech32ibc_prost/bech32ibc.bech32ibc.v1beta1.rs");
+        }
+    }
+}
+
+/// Ethermint protobuf definitions
+#[cfg(feature = "ethermint")]
+#[cfg_attr(docsrs, doc(cfg(feature = "ethermint")))]
+pub mod ethermint {
+    /// Ethermint EthSecp256k1 support
+    pub mod crypto {
+        pub mod v1 {
+            pub mod ethsecp256k1 {
+                include!("ethermint_prost/ethermint.crypto.v1.ethsecp256k1.rs");
+            }
+        }
+    }
+    /// EVM support
+    pub mod evm {
+        pub mod v1 {
+            include!("ethermint_prost/ethermint.evm.v1.rs");
+        }
+    }
+    /// Feemarket support
+    pub mod feemarket {
+        pub mod v1 {
+            include!("ethermint_prost/ethermint.feemarket.v1.rs");
+        }
+    }
+    /// Ethermint types
+    pub mod types {
+        pub mod v1 {
+            include!("ethermint_prost/ethermint.types.v1.rs");
+        }
+    }
+}
