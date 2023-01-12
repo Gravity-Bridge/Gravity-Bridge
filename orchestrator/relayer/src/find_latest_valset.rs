47337f93bed215807053d742ef0c2ec5cac0d03e
Pham Tu <tubackkhoa@gmail.com>
2023-01-12 07:30:49 +0700

    fix valset cache

--- orchestrator/relayer/src/find_latest_valset.rs
+++ orchestrator/relayer/src/find_latest_valset.rs
@@ -105,9 +105,6 @@ pub async fn find_latest_valset(
         current_block = end_search;
     }
 
-    // cache latest_eth_valset and current_block
-    set_latest_valset_info(evm_chain_prefix, (current_block, latest_eth_valset.clone()));
-
     // return cached valset
     if let Some(latest_eth_valset) = previous_valset.clone() {
         // cache latest_eth_valset and current_block
