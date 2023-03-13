d5b2ba64bf8ec986c2e0e6566594bf8c2735e7a7
Pham Tu <tubackkhoa@gmail.com>
2023-03-13 16:43:30 +0700

    done sample

--- orchestrator/relayer/examples/tron_rpc.rs
+++ orchestrator/relayer/examples/tron_rpc.rs
@@ -1,4 +1,4 @@
-use heliosphere::{core::Address, RpcClient};
+use heliosphere::{core::Address, signer::keypair::Keypair, signer::signer::Signer, RpcClient};
 use std::time::Duration;
 use web30::client::Web3;
 
@@ -26,7 +26,27 @@ pub async fn main() {
         web30::utils::get_base58_address("0xf2846a1e4dafaea38c1660a618277d67605bd2b5")
             .parse()
             .unwrap();
-
     let ret = client.get_account_balance(&address).await;
     println!("{:?}", ret);
+
+    let keypair = Keypair::from_hex_key(option_env!("PRIV_KEY").unwrap()).unwrap();
+    let mut trade_obj = client
+        .trx_transfer(
+            &"TPwTVfDDvmWSawsP7Ki1t3ecSBmaFeMMXc"
+                .parse::<Address>()
+                .unwrap(),
+            &"TY5X9ocQACH9YGAyiK3WUxLcLw3t2ethnc"
+                .parse::<Address>()
+                .unwrap(),
+            100u64,
+        )
+        .await
+        .unwrap();
+
+    keypair.sign_transaction(&mut trade_obj).unwrap();
+
+    println!("{:?}", serde_json::to_string(&trade_obj).unwrap());
+
+    // broad cast
+    // client.broadcast_transaction(&trade_obj).await.unwrap();
 }
