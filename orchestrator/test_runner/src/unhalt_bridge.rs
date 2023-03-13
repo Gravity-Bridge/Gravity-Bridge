bf2ed0ef1c7554190254a315d7f9aaa75bf2720c
Christian Borst <christianoborst@gmail.com>
2023-03-13 14:46:42 -0400

    WIP: Fix the parse strings in unhalt bridge

--- orchestrator/test_runner/src/unhalt_bridge.rs
+++ orchestrator/test_runner/src/unhalt_bridge.rs
@@ -143,7 +143,7 @@ pub async fn unhalt_bridge_test(
 
     info!("Checking that bridge is halted!");
 
-    let halted_bridge_amt = Uint256::from_str("100_000_000_000_000_000").unwrap();
+    let halted_bridge_amt = Uint256::from_str("100000000000000000").unwrap();
     // Attempt transaction on halted bridge
     let res = test_erc20_deposit_result(
         web30,
@@ -215,7 +215,7 @@ pub async fn unhalt_bridge_test(
     info!("Observing attestations before bridging asset to cosmos!");
     print_sends_to_cosmos(&grpc_client, true).await;
 
-    let fixed_bridge_amt = Uint256::from_str("50_000_000_000_000_000").unwrap();
+    let fixed_bridge_amt = Uint256::from_str("50000000000000000").unwrap();
     info!("Attempting to resend now that the bridge should be fixed");
     // After the reset, we checked that the deposit submitted during the period when the bridge
     // was halted has been bridged after the chain was unhalted. Increase for the new deposit call
