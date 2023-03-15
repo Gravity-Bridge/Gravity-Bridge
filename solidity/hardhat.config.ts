f207580083678d8be254d015938441e13f72a25c
Pham Tu <tubackkhoa@gmail.com>
2023-03-15 11:51:34 +0700

    add account init

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -2,32 +2,44 @@ import "@nomiclabs/hardhat-waffle";
 import "hardhat-gas-reporter";
 import "hardhat-typechain";
 import "hardhat-contract-sizer";
-import { task } from "hardhat/config";
-import { HardhatConfig } from "hardhat/types";
+import { task, extendEnvironment } from "hardhat/config";
+import {
+  HardhatNetworkAccountsUserConfig,
+  HardhatUserConfig,
+} from "hardhat/types";
+import { ethers } from "ethers";
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = hre.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(await account.getAddress());
+    // console.log(await account.getAddress());
   }
 });
 
-// // This is a sample Buidler task. To learn how to create your own go to
-// // https://buidler.dev/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
-//   const accounts = await bre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(await account.getAddress());
-//   }
-// });
+let accounts: HardhatNetworkAccountsUserConfig | undefined = undefined;
+if (process.env.MNEMONIC) {
+  accounts = {
+    mnemonic: process.env.MNEMONIC,
+    path: "m/44'/60'/0'/0",
+    initialIndex: 0,
+    count: 20,
+    passphrase: "",
+  };
+} else if (process.env.PRIVATE_KEY) {
+  accounts = process.env.PRIVATE_KEY.split(/\s*,\s*/).map((pv) => ({
+    privateKey: pv,
+    balance: "10000000000000000000000",
+  }));
+}
 
 // You have to export an object to set up your config
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
-export default {
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   // This is a sample solc configuration that specifies which version of solc to use
   solidity: {
     version: "0.8.10",
@@ -40,13 +52,7 @@ export default {
   networks: {
     hardhat: {
       chainId: 420,
-      accounts: [
-        {
-          privateKey:
-            "0xbbfb76c92cd13796899f63dc6ead6d2420e8d0bc502d42bd5773c2d4b8897f08",
-          balance: "10000000000000000000000",
-        },
-      ],
+      accounts,
       forking: {
         url: "https://rpc.ankr.com/eth_goerli",
         blockNumber: 8218229,
@@ -60,7 +66,6 @@ export default {
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
-    runOnCompile: true,
   },
   gasReporter: {
     enabled: true,
@@ -68,4 +73,24 @@ export default {
   mocha: {
     timeout: 2000000,
   },
-} as unknown as HardhatConfig;
+};
+
+declare module "hardhat/types/runtime" {
+  export interface HardhatRuntimeEnvironment {
+    provider: ethers.providers.Web3Provider;
+    getSigner: (
+      addressOrIndex?: string | number
+    ) => ethers.providers.JsonRpcSigner;
+    getSigners: (num?: number) => ethers.providers.JsonRpcSigner[];
+  }
+}
+
+extendEnvironment((hre) => {
+  // @ts-ignore
+  hre.provider = new ethers.providers.Web3Provider(hre.network.provider);
+  hre.getSigners = (num = 20) =>
+    [...new Array(num)].map((_, i) => hre.provider.getSigner(i));
+  hre.getSigner = (addressOrIndex) => hre.provider.getSigner(addressOrIndex);
+});
+
+export default config;
