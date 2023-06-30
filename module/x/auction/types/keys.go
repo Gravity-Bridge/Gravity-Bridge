3f846251c67006329d9b3b4655cbe755b9c8dafa
hoangdv2429 <hoangdv2429@gmail.com>
2023-07-01 06:27:51 +0700

    init key

---
+++ module/x/auction/types/keys.go
@@ -0,0 +1,36 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "auction"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for slashing
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	MemStoreKey = "mem_auction"
+
+	// Version defines the current version the IBC module supports
+	Version = "auction-1"
+
+	// PortID is the default port id that module binds to
+	PortID = "auction"
+
+	// Auction
+	// KeyPrefixAuction is the key used to store the auction in the KVStore
+	KeyPrefixAuction = "auction-value-"
+
+	// Bid
+	// KeyPrefixBid defines the prefix to store bid
+	KeyPrefixBid = "bid-value-"
+
+	// AuctionPeriod
+	// KeyPrefixAuctionPeriod defines the prefix to store auction period
+	KeyPrefixAuctionPeriod = "auctionPeriod-value-"
+)
