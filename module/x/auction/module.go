2732af052590a24b49b50f98d6fa36994d71540d
sontrinh16 <trinhleson2000@gmail.com>
2023-07-04 11:00:07 +0700

    add app module basic structure

--- module/x/auction/module.go
+++ module/x/auction/module.go
@@ -1,1 +1,188 @@
 package auction
+
+import (
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	authKeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/keeper"
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+)
+
+// type check to ensure the interface is properly implemented
+// nolint: exhaustruct
+var (
+	_ module.AppModule      = AppModule{}
+	_ module.AppModuleBasic = AppModuleBasic{}
+)
+
+// AppModuleBasic object for module implementation
+type AppModuleBasic struct{}
+
+// Name implements app module basic
+func (AppModuleBasic) Name() string {
+	return types.ModuleName
+}
+
+// RegisterLegacyAminoCodec implements app module basic
+func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	// TODO: implement
+	// types.RegisterCodec(cdc)
+}
+
+// DefaultGenesis implements app module basic
+func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	// TODO: Implement
+	// return cdc.MustMarshalJSON(types.DefaultGenesisState())
+	return nil
+}
+
+// ValidateGenesis implements app module basic
+func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
+	// TODO: implement
+	// var data types.GenesisState
+	// if err := cdc.UnmarshalJSON(bz, &data); err != nil {
+	// 	return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
+	// }
+
+	// return data.ValidateBasic()
+	return nil
+}
+
+// RegisterRESTRoutes implements app module basic
+func (AppModuleBasic) RegisterRESTRoutes(ctx client.Context, rtr *mux.Router) {
+	// TODO: Implement
+	// rest.RegisterRoutes(ctx, rtr, types.StoreKey)
+}
+
+// GetQueryCmd implements app module basic
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	// TODO: implement
+
+	// return cli.GetQueryCmd()
+	return nil
+}
+
+// GetTxCmd implements app module basic
+func (AppModuleBasic) GetTxCmd() *cobra.Command {
+	// TODO: implement
+
+	// return cli.GetTxCmd(types.StoreKey)
+	return nil
+}
+
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the distribution module.
+// also implements app module basic
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+	// TODO: Implement
+	// err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	//
+	//	if err != nil {
+	//		panic("Failed to register query handler")
+	//	}
+}
+
+// RegisterInterfaces implements app module basic
+func (b AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	// TODO: Implement
+	// types.RegisterInterfaces(registry)
+}
+
+// ___________________________________________________________________________
+
+// AppModule object for module implementation
+type AppModule struct {
+	AppModuleBasic
+	keeper        keeper.Keeper
+	bankKeeper    bankkeeper.Keeper
+	accountKeeper authKeeper.AccountKeeper
+}
+
+func (am AppModule) ConsensusVersion() uint64 {
+	return 1
+}
+
+// NewAppModule creates a new AppModule Object
+func NewAppModule(k keeper.Keeper, bankKeeper bankkeeper.Keeper, accountkeeper authKeeper.AccountKeeper) AppModule {
+	return AppModule{
+		AppModuleBasic: AppModuleBasic{},
+		keeper:         k,
+		bankKeeper:     bankKeeper,
+		accountKeeper:  accountkeeper,
+	}
+}
+
+// Name implements app module
+func (AppModule) Name() string {
+	return types.ModuleName
+}
+
+// RegisterInvariants implements app module
+func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
+	// TODO: Implement
+}
+
+// Route implements app module
+func (am AppModule) Route() sdk.Route {
+	// TODO: Implement
+	// return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper))
+	return sdk.Route{}
+}
+
+// QuerierRoute implements app module
+func (am AppModule) QuerierRoute() string {
+	return types.QuerierRoute
+}
+
+// LegacyQuerierHandler returns the distribution module sdk.Querier.
+func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+	// TODO: Implement
+	// return keeper.NewQuerier(am.keeper)
+	return nil
+}
+
+// RegisterServices registers module services.
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	// TODO: Register msgServer
+	// types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+	// types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+}
+
+// InitGenesis initializes the genesis state for this module and implements app module.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
+	// TODO: implement
+	// var genesisState types.GenesisState
+	// cdc.MustUnmarshalJSON(data, &genesisState)
+	// keeper.InitGenesis(ctx, am.keeper, genesisState)
+	return []abci.ValidatorUpdate{}
+}
+
+// ExportGenesis exports the current genesis state to a json.RawMessage
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	// TODO: implement
+	// gs := keeper.ExportGenesis(ctx, am.keeper)
+	// return cdc.MustMarshalJSON(&gs)
+	return nil
+}
+
+// BeginBlock implements app module
+func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
+	BeginBlocker(ctx, am.keeper, am.bankKeeper, am.accountKeeper)
+}
+
+// EndBlock implements app module
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	EndBlocker(ctx, am.keeper, am.bankKeeper, am.accountKeeper)
+	return []abci.ValidatorUpdate{}
+}
