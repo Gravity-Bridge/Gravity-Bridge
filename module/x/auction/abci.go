b3f3de1d0983eeeeee1b9d2bf5dd82bdb4427a5d
sontrinh16 <trinhleson2000@gmail.com>
2023-07-04 10:09:25 +0700

    add end blocker

--- module/x/auction/abci.go
+++ module/x/auction/abci.go
@@ -58,7 +58,27 @@ func StartMewAuctionPeriod(ctx sdk.Context, params types.Params, k keeper.Keeper
 
 }
 
-func EndAuctionPeriod(ctx sdk.Context, params types.Params, k keeper.Keeper, bk types.BankKeeper, ak types.AccountKeeper) error {
+func EndAuctionPeriod(
+	ctx sdk.Context,
+	params types.Params,
+	latestAuctionPeriod types.AuctionPeriod,
+	k keeper.Keeper,
+	bk types.BankKeeper,
+	ak types.AccountKeeper,
+) error {
+	for _, auction := range latestAuctionPeriod.Auctions {
+		if auction.HighestBid == nil {
+			err := k.SendToCommunityPool(ctx, sdk.Coins{*auction.AuctionAmount})
+			if err != nil {
+				panic(err)
+			}
+			continue
+		}
+
+		// Send in the winning token to the highest bidder address
+		bk.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sdk.AccAddress(auction.HighestBid.BidderAddress), sdk.Coins{*auction.AuctionAmount})
+	}
+
 	return nil
 }
 
@@ -73,6 +93,23 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper, bk types.BankKeeper, ak type
 
 	if uint64(ctx.BlockHeight())-lastAuctionPeriods.StartBlockHeight == params.AuctionEpoch {
 		err := StartMewAuctionPeriod(ctx, params, k, bk, ak)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func EndBlocker(ctx sdk.Context, k keeper.Keeper, bk types.BankKeeper, ak types.AccountKeeper) {
+	params := k.GetParams(ctx)
+
+	// An initial auction period need to be set as a starting point
+	lastAuctionPeriods, found := k.GetLatestAuctionPeriod(ctx)
+	if !found {
+		return
+	}
+
+	if lastAuctionPeriods.StartBlockHeight-uint64(ctx.BlockHeight()) == params.AuctionPeriod {
+		err := EndAuctionPeriod(ctx, params, *lastAuctionPeriods, k, bk, ak)
 		if err != nil {
 			return
 		}
