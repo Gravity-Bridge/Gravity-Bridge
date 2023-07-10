00fb52df798ee5f75732442c33b14199326aaaed
hoangdv2429 <hoangdv2429@gmail.com>
2023-07-10 15:19:47 +0700

    change to Bid and Auction struct

--- module/x/auction/keeper/bid.go
+++ module/x/auction/keeper/bid.go
@@ -17,29 +17,3 @@ func (k Keeper) GetBidByID(ctx sdk.Context, auctionID uint64) (val types.Bid, fo
 	k.cdc.MustUnmarshal(bz, &val)
 	return val, true
 }
-
-// SetBid sets the bid for a specific auction and bidder.
-func (k Keeper) SetBid(ctx sdk.Context, auctionID uint64, bid types.Bid) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.KeyPrefixBid))
-	bidKey := append(uint64ToByte(auctionID), []byte(bid.BidderAddress)...)
-	bz := k.cdc.MustMarshal(&bid)
-	store.Set(bidKey, bz)
-}
-
-// UpdateBidAmount updates the bid amount for a specific auction and bidder.
-func (k Keeper) UpdateBidAmount(ctx sdk.Context, auctionID uint64, newAmount sdk.Coin) bool {
-	bid, found := k.GetBidByID(ctx, auctionID)
-	if !found {
-		return false
-	}
-	bid.BidAmount = &newAmount
-	k.SetBid(ctx, auctionID, bid)
-	return true
-}
-
-// DeleteBid deletes the bid for a specific auction and bidder.
-func (k Keeper) DeleteBid(ctx sdk.Context, auctionID uint64, bidderAddress string) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(types.KeyPrefixBid))
-	bidKey := append(uint64ToByte(auctionID), []byte(bidderAddress)...)
-	store.Delete(bidKey)
-}
