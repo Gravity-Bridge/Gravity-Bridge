d0bafef718603617fd0ff293b87e51a930fe1709
hoangdv2429 <hoangdv2429@gmail.com>
2023-07-10 15:22:48 +0700

    minor

--- module/x/auction/types/errors.go
+++ module/x/auction/types/errors.go
@@ -4,7 +4,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// x/stakeibc module sentinel errors
+// x/auction module sentinel errors
 var (
 	ErrNoPreviousAuctionPeriod = sdkerrors.Register(ModuleName, 1, "Previous auction period not found")
 	ErrInvalidBidAmountGap     = sdkerrors.Register(ModuleName, 2, "Invalid bid amount gap")
