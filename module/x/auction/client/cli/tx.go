08b2531df5bb11ad8623e01d36bb908d08f5a0a2
hoangdv2429 <hoangdv2429@gmail.com>
2023-07-11 17:58:40 +0700

    cmd tx bid

--- module/x/auction/client/cli/tx.go
+++ module/x/auction/client/cli/tx.go
@@ -1,1 +1,25 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd() *cobra.Command {
+	auctionTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	auctionTxCmd.AddCommand(CmdMsgBid())
+
+	return auctionTxCmd
+}
---
+++ module/x/auction/client/cli/tx_bid.go
@@ -0,0 +1,54 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/spf13/cobra"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+)
+
+func CmdMsgBid() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "bid [auction-id] [amount]",
+		Short: "Broadcast message bid",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			auctionId, err := strconv.Atoi(args[0])
+			if err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			argAmount, err := sdk.ParseCoinNormalized(args[1])
+			if err != nil {
+				return fmt.Errorf("invalid coins: %s", err)
+			}
+
+			msg := types.NewMsgBid(
+				uint64(auctionId),
+				clientCtx.GetFromAddress().String(),
+				argAmount,
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
