19ee779638088fffd537341debce89cca87e0603
sontrinh16 <trinhleson2000@gmail.com>
2023-07-12 02:58:30 +0700

    update getTxCmd in module.go

--- module/x/auction/module.go
+++ module/x/auction/module.go
@@ -71,10 +71,7 @@ func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 
 // GetTxCmd implements app module basic
 func (AppModuleBasic) GetTxCmd() *cobra.Command {
-	// TODO: implement
-
-	// return cli.GetTxCmd(types.StoreKey)
-	return nil
+	return cli.GetTxCmd()
 }
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the distribution module.
