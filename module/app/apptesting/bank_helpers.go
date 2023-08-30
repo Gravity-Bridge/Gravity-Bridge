8b02337b3b8b9e516b3ee088b2a57163de9f0a1a
Christian Borst <christianoborst@gmail.com>
2023-08-30 12:29:04 -0400

    Add app-based unit testing helpers
    This change was made with the help of the Notional team, who created the
    initial version of the auction module.
    
    Co-authored-by: sontrinh16 <48055119+sontrinh16@users.noreply.github.com>
    Co-authored-by: Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
    Co-authored-by: Long <77162856+mgl2150@users.noreply.github.com>
    Co-authored-by: vuong177 <nguyenvuong1122000@gmail.com>
    Co-authored-by: DongLieu <duongduydongtq@gmail.com>
    Co-authored-by: Đông Liều <93205232+DongLieu@users.noreply.github.com>
    Co-authored-by: Đỗ Việt Hoàng <hoangdv2429@gmail.com>
    Co-authored-by: Char Lyes <dungnt183721@gmail.com>
    Co-authored-by: Tien Dung <neitdung@gmail.com>
    Co-authored-by: DongCoNY <dong183497@gmail.com>

---
+++ module/app/apptesting/bank_helpers.go
@@ -0,0 +1,52 @@
+package apptesting
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+)
+
+// Mints `amounts` and sends them to `addr`
+func (s *AppTestHelper) FundAccount(ctx sdk.Context, addr sdk.AccAddress, amounts sdk.Coins) {
+	bankkeeper := s.App.AuctionKeeper.BankKeeper
+	bankkeeper.MintCoins(ctx, minttypes.ModuleName, amounts)
+	bankkeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, addr, amounts)
+}
+
+// Mints `amounts` and sends them to the module with the given `moduleName`
+func (s *AppTestHelper) FundModule(ctx sdk.Context, moduleName string, amounts sdk.Coins) {
+	bankkeeper := s.App.AuctionKeeper.BankKeeper
+	bankkeeper.MintCoins(ctx, minttypes.ModuleName, amounts)
+	bankkeeper.SendCoinsFromModuleToModule(ctx, minttypes.ModuleName, moduleName, amounts)
+}
+
+// Mints tokens and places them in the community pool
+func (s *AppTestHelper) FundCommunityPool(ctx sdk.Context, amounts sdk.Coins) {
+	decAmounts := sdk.NewDecCoinsFromCoins(amounts...)
+	distKeeper := s.App.AuctionKeeper.DistKeeper
+	s.FundModule(ctx, distrtypes.ModuleName, amounts)
+	communityPool := distKeeper.GetFeePool(ctx)
+	communityPool.CommunityPool = communityPool.CommunityPool.Add(decAmounts...)
+
+	distKeeper.SetFeePool(ctx, communityPool)
+}
+
+// Creates `numAccounts` accounts and mints for them the specified `amounts`
+func (s *AppTestHelper) CreateAndFundRandomAccounts(numAccounts int, amounts sdk.Coins) []sdk.AccAddress {
+	addrs := CreateRandomAccounts(numAccounts)
+
+	for _, addr := range addrs {
+		s.FundAccount(s.Ctx, addr, amounts)
+
+		// Assert that the balances actually updated
+		for _, coin := range amounts {
+			balance := s.App.BankKeeper.GetBalance(s.Ctx, addr, coin.Denom)
+			if !balance.IsGTE(coin) {
+				panic("Invalid balance")
+			}
+		}
+	}
+	s.TestAccs = addrs
+
+	return addrs
+}
