1ade38ec25d929676f97340c714f54f75577e84c
Christian Borst <christianoborst@gmail.com>
2023-09-12 11:25:16 -0400

    Add auction module CLI
    This change was made with the help of the Notional team, who created the
    initial version of the auction module.
    
    Co-authored-by: sontrinh16 <48055119+sontrinh16@users.noreply.github.com>
    Co-authored-by: Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
    Co-authored-by: Long <77162856+mgl2150@users.noreply.github.com>
    Co-authored-by: vuong177 <nguyenvuong1122000@gmail.com>
    Co-authored-by: DongLieu <duongduydongtq@gmail.com>
    Co-authored-by: Đông Liều <93205232+DongLieu@users.noreply.github.com>
    Co-authored-by: Đỗ Việt Hoàng <hoangdv2429@gmail.com>
    Co-authored-by: Char Lyes <dungnt183721@gmail.com>
    Co-authored-by: Tien Dung <neitdung@gmail.com>
    Co-authored-by: DongCoNY <dong183497@gmail.com>

---
+++ module/x/auction/client/cli/query.go
@@ -0,0 +1,225 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd() *cobra.Command {
+	// Group auction queries under a subcommand
+	// nolint: exhaustruct
+	auctionQueryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	auctionQueryCmd.AddCommand([]*cobra.Command{
+		GetCmdParams(),
+		GetCmdAuctionPeriod(),
+		GetCmdAuctions(),
+		GetCmdAuctionById(),
+		GetCmdAuctionByDenom(),
+		GetCmdAllAuctionsByBidder(),
+	}...)
+
+	return auctionQueryCmd
+}
+func GetCmdParams() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Fetch the auction module params",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdAuctionPeriod fetches the current auction period
+func GetCmdAuctionPeriod() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "auction-period",
+		Args:  cobra.NoArgs,
+		Short: "Query auction periods by id",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AuctionPeriod(cmd.Context(), &types.QueryAuctionPeriodRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdAuctions fetches all auctions in the store
+func GetCmdAuctions() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "auctions",
+		Args:  cobra.NoArgs,
+		Short: "Fetch active auctions",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Auctions(cmd.Context(), &types.QueryAuctionsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdAuctionByDenom fetches an auction with the given denom, if any exists
+func GetCmdAuctionByDenom() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "auction-by-denom [denom]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Fetch an active auction with the given denom",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			denom := args[0]
+			err = sdk.ValidateDenom(denom)
+			if err != nil {
+				return sdkerrors.Wrap(err, "Invalid query denom")
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AuctionByDenom(cmd.Context(), &types.QueryAuctionByDenomRequest{
+				AuctionDenom: denom,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdAuctionById fetches for an auction with the given id, if any exists
+func GetCmdAuctionById() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "auction-by-id [id]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Fetch an active auction with the given id",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return sdkerrors.Wrap(err, "Invalid query id")
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AuctionById(cmd.Context(), &types.QueryAuctionByIdRequest{
+				AuctionId: id,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdAllAuctionsByBidder all the auctions for which bidderAddress is the current highest bidder
+func GetCmdAllAuctionsByBidder() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "auctions-by-bidder [bidderAddress]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Fetch all auctions where bidderAddress is the current highest bidder",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			address := args[0]
+			accAddress, err := sdk.AccAddressFromBech32(address)
+			if err != nil {
+				return sdkerrors.Wrap(err, "Invalid query address")
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AllAuctionsByBidder(cmd.Context(), &types.QueryAllAuctionsByBidderRequest{
+				Address: accAddress.String(),
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
