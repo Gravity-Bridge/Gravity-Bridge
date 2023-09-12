a4dbf58317da11ada647e6f604fd41addcbffbda
Christian Borst <christianoborst@gmail.com>
2023-09-12 11:25:16 -0400

    Add auction module CLI
    This change was made with the help of the Notional team, who created the
    initial version of the auction module.
    
    Co-authored-by: sontrinh16 <48055119+sontrinh16@users.noreply.github.com>
    Co-authored-by: Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
    Co-authored-by: Long <77162856+mgl2150@users.noreply.github.com>
    Co-authored-by: vuong177 <nguyenvuong1122000@gmail.com>
    Co-authored-by: DongLieu <duongduydongtq@gmail.com>
    Co-authored-by: Đông Liều <93205232+DongLieu@users.noreply.github.com>
    Co-authored-by: Đỗ Việt Hoàng <hoangdv2429@gmail.com>
    Co-authored-by: Char Lyes <dungnt183721@gmail.com>
    Co-authored-by: Tien Dung <neitdung@gmail.com>
    Co-authored-by: DongCoNY <dong183497@gmail.com>

---
+++ module/x/auction/client/cli/tx_bid.go
@@ -0,0 +1,97 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+
+	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+)
+
+func CmdMsgBid() *cobra.Command {
+	// nolint: exhaustruct
+	cmd := &cobra.Command{
+		Use:   "bid [auction-id] [amount] [optional fee]",
+		Short: "Submit a bid on an auction: bid MUST be the new highest bid or will be rejected. Automatically supplies the minimum fee according to the module parameters.",
+		Args:  cobra.RangeArgs(2, 3),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			auctionId, err := strconv.Atoi(args[0])
+			if err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			// If the user provided a custom fee, use that. Otherwise fetch the minimum and provide that amount.
+			var bidFee uint64
+			if len(args) == 2 {
+				queryCtx, err := client.GetClientQueryContext(cmd)
+				if err != nil {
+					return err
+				}
+				queryClient := types.NewQueryClient(queryCtx)
+				res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+				if err != nil {
+					return err
+				}
+				bidFee = res.Params.MinBidFee
+			} else {
+				intFee, err := strconv.Atoi(args[2])
+				if err != nil {
+					return err
+				}
+				bidFee = uint64(intFee)
+			}
+
+			argAmount, err := strconv.Atoi(args[1])
+			if err != nil {
+				return fmt.Errorf("invalid amount: %v", err)
+			}
+
+			offline, err := cmd.Flags().GetBool(flags.FlagOffline)
+			if err != nil {
+				return fmt.Errorf("unable to read offline flag: %v", err)
+			}
+
+			if !clientCtx.Offline && !offline {
+				// Check the current auction highest bid and error if bid is too low
+				queryCtx, err := client.GetClientQueryContext(cmd)
+				if err != nil {
+					return err
+				}
+				queryClient := types.NewQueryClient(queryCtx)
+				res, err := queryClient.AuctionById(cmd.Context(), &types.QueryAuctionByIdRequest{AuctionId: uint64(auctionId)})
+				if err != nil {
+					return err
+				}
+				if res.Auction.HighestBid != nil && res.Auction.HighestBid.BidAmount >= uint64(argAmount) {
+					return fmt.Errorf("bid amount (%d) is lower than current highest bid %v", argAmount, res.Auction.HighestBid)
+				}
+			}
+
+			msg := types.NewMsgBid(
+				uint64(auctionId),
+				clientCtx.GetFromAddress().String(),
+				uint64(argAmount),
+				bidFee,
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
