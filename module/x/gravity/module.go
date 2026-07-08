ca9eb9f8f2ed74ba4a1230332337dca916174b8b
Christian <christianoborst@gmail.com>
2026-07-08 13:03:45 -0400

    Register AttestationHashIntegrityInvariant

--- module/x/gravity/module.go
+++ module/x/gravity/module.go
@@ -132,6 +132,7 @@ func (AppModule) Name() string {
 // RegisterInvariants implements app module
 func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
 	ir.RegisterRoute(types.ModuleName, "module-balance", keeper.ModuleBalanceInvariant(am.keeper))
+	ir.RegisterRoute(types.ModuleName, "attestation-hash-integrity", keeper.AttestationHashIntegrityInvariant(am.keeper))
 	ir.RegisterRoute(types.ModuleName, "store-validity", keeper.StoreValidityInvariant(am.keeper))
 }
 
